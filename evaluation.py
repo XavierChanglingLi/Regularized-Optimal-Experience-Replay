d9bb5e4bd41ec4fb653a07e3371acdde1a8f8398
ChanglingLi <59809140+XavierChanglingLi@users.noreply.github.com>
2024-07-04 11:52:20 +0200

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -28,23 +28,3 @@ def evaluate(agent, env: gym.Env, num_episodes: int) -> Dict[str, float]:
         stats['success'] = successes / num_episodes
     return stats
 
-def value_estimate(agent, observations, Qpos, Qvel, discount: float, env: gym.Env) -> Dict[str, float]:
-    stats = {'True_value': []}
-    total_mc_return = 0.0
-    for i in range(observations.shape[0]):
-        _, done = env.reset(), False
-        observation = observations[i]
-        env.set_state(Qpos[i], Qvel[i])
-        mc_return = 0.0
-        total_steps = 0
-        while not done or total_steps <= 1000:
-            action = agent.sample_actions(observation, temperature=0.0)
-            observation, reward, done, info = env.step(action)
-            mc_return += reward *(discount**total_steps)
-            total_steps += 1
-        total_mc_return += mc_return
-    value_estimate = total_mc_return/(observations.shape[0])
-    stats['True_value'] = value_estimate
-    return stats
-        
-
